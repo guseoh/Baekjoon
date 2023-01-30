b387041d79182d0162b85e925a5017dc2bf7407e
guseoh <guseoh@naver.com>
2023-01-30 20:46:25 +0900

    commit

---
+++ 2023301.29.py
@@ -0,0 +1,17 @@
+# n = int(input())
+# for i in range(1,n+1):
+#     print(" " * (n-i) + "*" * (2 * i - 1))
+
+# while True:
+#     a,b = map(int, input().split())
+#     if a == 0 and b == 0:
+#         break
+    
+#     if a % b == 0:
+#         print("multiple")
+#     elif b % a == 0:
+#         print("factor")
+#     else: print("neither")
+
+a,i = map(int, input().split())
+print(a * (i - 1)+ 1)
