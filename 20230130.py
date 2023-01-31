c4fc6e552d1d60a70be6eb6ca392f9e96adb075a
guseoh <guseoh@naver.com>
2023-01-31 23:32:42 +0900

    commit

--- 20230130.py
+++ 20230130.py
@@ -14,17 +14,30 @@
 # aa.sort()
 # print(aa[b-1])
 
-n = int(input())
-li = list(map(int, input().split()))
-count = 0
+# n = int(input())
+# li = list(map(int, input().split()))
+# count = 0
 
-for i in range(n):
-    error = 0
-    if li[i] > 1:
-        for j in range(2,li[i]):
-            if li[i] % j == 0:
-                error += 1
-        if error == 0:
-            count += 1
+# for i in range(n):
+#     error = 0
+#     if li[i] > 1:
+#         for j in range(2,li[i]):
+#             if li[i] % j == 0:
+#                 error += 1
+#         if error == 0:
+#             count += 1
 
-print(count)
+# print(count)
+
+# em = list(map(int, input().split()))
+# aa = []
+# for i in range(len(em)):
+#     aa.append(em.index(em[i]))
+# print(aa)
+
+def solution(n):  # extend
+    answer = []
+    for i in range(1,n+1):
+        if n % i == 0:
+            answer.extend([(i, n//i)])
+    return len(answer)
