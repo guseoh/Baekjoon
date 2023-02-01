143fc3996f5ae4ab6cd87169d383fcebaa2d8732
guseoh <guseoh@naver.com>
2023-02-02 00:09:37 +0900

    commit

--- 20230201.py
+++ 20230201.py
@@ -19,3 +19,27 @@
 #     print(1)
 # else: print(0)
 
+import sys
+sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+n,m = map(int, input().split())
+
+graph = [ [] for _ in range(n+1)]
+visited = [False] * (n+1)
+
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+def dfs(v):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i) # 재귀
+count = 0
+for i in range(1,n+1):
+    if not visited[i]:
+        count += 1
+        dfs(i)
+print(count)
