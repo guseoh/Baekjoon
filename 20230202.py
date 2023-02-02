18fb3e62354fdcf60e86485632f4bdf53b3497fe
guseoh <guseoh@naver.com>
2023-02-03 03:03:49 +0900

    commit

--- 20230202.py
+++ 20230202.py
@@ -1,9 +1,38 @@
-import re
-a = []
-sum = 0
-ss = "abc1adad2"
-num = re.sub(r'[^0-9]','',ss)
-for i in num:
-    sum += int(i)
+# import re
+# a = []
+# sum = 0
+# ss = "abc1adad2"
+# num = re.sub(r'[^0-9]','',ss)
+# for i in num:
+#     sum += int(i)
 
-print(sum)
+# print(sum)
+
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# answer = list(map(int, input().split()))
+
+# nn = int(input())
+# answer2 = list(map(int, input().split()))
+
+# for i in answer2:
+#     cnt = 0
+#     for j in range(n):
+#         if answer[j] == i:
+#             cnt += 1
+#             break
+#     if cnt != 0:
+#         print(1)
+#     else: print(0)
+
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
+# answer = set(map(int, input().split()))
+# nn = int(input())
+# answer2 = list(map(int, input().split()))
+
+# for i in answer2:
+#     print(1) if i in answer else print(0)
