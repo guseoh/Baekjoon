90445820f81360b18d2ee1fd41ca569d39705862
guseoh <guseoh@naver.com>
2023-02-15 22:35:49 +0900

    commit

--- 20230214.py
+++ 20230214.py
@@ -23,28 +23,71 @@
 
 
 # 완전 탐색
-n,m = map(int, input().split())
-board = []
+# n,m = map(int, input().split())
+# board = []
+# answer = []
+
+# for _ in range(n):
+#     board.append(input())
+
+# for col in range(n - 7):
+#     for row in range(m - 7):
+#         w = 0 # White
+#         b = 0 # Black
+#         for i in range(col, col + 8):
+#             for j in range(row, row + 8):
+#                 if (i + j) % 2 == 0:
+#                     if board[i][j] != 'W':
+#                         w += 1
+#                     if board[i][j] != 'B':
+#                         b += 1
+#                 else:
+#                     if board[i][j] != 'B':
+#                         w += 1
+#                     if board[i][j] != 'W':
+#                         b += 1
+#         answer.append(w)
+#         answer.append(b)
+
+# print(min(answer))
+
+# N, M = map(int, input().split())
+# original = []
+# count = []
+
+# for _ in range(N):
+#     original.append(input())
+
+# for a in range(N-7):
+#     for b in range(M-7):
+#         index1 = 0
+#         index2 = 0
+#         for i in range(a, a+8):
+#             for j in range(b, b+8):
+#                 if (i+j) % 2 == 0:
+#                     if original[i][j] != 'W':
+#                         index1 += 1
+#                     if original[i][j] != 'B':
+#                         index2 += 1
+#                 else:
+#                     if original[i][j] != 'B':
+#                         index1 += 1
+#                     if original[i][j] != 'W':
+#                         index2 += 1
+#         count.append(min(index1, index2))
+
+# print(min(count))
+
+n, m = map(int, input().split())
 answer = []
 
-for _ in range(n):
-    board.append(input())
-for col in range(n - 7):
-    for row in range(m - 7):
-        w = 0 # White
-        b = 0 # Black
-        for i in range(col,col+8):
-            for j in range(row, row+8):
-                if (i + j) % 2 == 0:
-                    if board[i][j] != 'W':
-                        w += 1
-                    if board[i][j] != 'B':
-                        b += 1
-                else:
-                    if board[i][j] != 'W':
-                        w += 1
-                    if board[i][j] != 'B':
-                        b += 1
-        answer.append(w)
-        answer.append(b)
-print(min(answer))
+def back():
+    if len(answer) == m:
+        print(' '.join(map(str, answer)))
+        return
+    for i in range(1, n+1):
+        if i not in answer:
+            answer.append(i)
+            back()
+            answer.pop()
+back()
---
+++ 20230215.py
@@ -0,0 +1,34 @@
+# BFS => Queue, DFS => 재귀함수
+# def dfs(idx, sum):
+#     global answer
+#     if(idx >= n):
+#         return
+#     sum += arr[idx]
+#     if(s == sum):
+#         answer += 1
+#     dfs(idx+1, sum - arr[idx])
+#     dfs(idx+1, sum) 
+    
+# n,s = map(int, input().split())
+# arr = list(map(int, input().split()))
+# answer = 0
+# dfs(0,0)
+# print(answer)
+
+# n,k = list(map(int, input().split()))
+
+# up = 1
+# for i in range(1, n+1): # n!
+#     up *= i
+
+# down = 1
+# for i in range(1, n-k+1): # (n-k)!
+#     down *= i
+
+# down2 = 1
+# for i in range(1,k+1): # k!
+#     down2 *= i
+
+# down *= down2 # (n-k)! * k!
+# print(up // down)
+
