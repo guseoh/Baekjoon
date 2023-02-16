e32f6f1b2b65edb7256594a03358c58e36d1e281
guseoh <guseoh@naver.com>
2023-02-16 22:55:44 +0900

    commit

--- 20230215.py
+++ 20230215.py
@@ -32,3 +32,14 @@
 # down *= down2 # (n-k)! * k!
 # print(up // down)
 
+def LCM(a, b):
+    return (a * b) // GCD(a, b)
+def GCD(a, b): # 유클리드 호제법
+    if b % a:
+        return GCD(b % a, a)
+    else: return a
+
+n = int(input())
+for i in range(n):
+    a,b = map(int, input().split())
+    print(LCM(a,b))
