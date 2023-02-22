e9d32fa4c84a34d0015006b08ca03cd01001d087
guseoh <guseoh@naver.com>
2023-02-22 22:10:51 +0900

    commit

--- 20230220.py
+++ 20230220.py
@@ -69,3 +69,87 @@ for n in num:
 se = num - rev
 for i in sorted(se):
     print(i)
+
+# 순열과 조합
+import itertools
+
+pool = ['A', 'B', 'C']
+print(list(map(''.join, itertools.permutations(pool)))) 
+print(list(map(''.join, itertools.combinations(pool, 2))))
+
+# import itertools
+
+# def solution(mylist):
+#     answer = []
+#     answer = ''.join(map(str, itertools.permutations(mylist)))
+#     return answer
+
+from itertools import permutations
+def solution(mylist):
+    return sorted(list(permutations(mylist, len(mylist))))
+
+def permute(mylist):
+    result = [mylist[:]]
+    c = [0] * len(mylist)
+    i = 0
+    while i < len(mylist):
+        if c[i] < i:
+            if i % 2 == 0:
+                mylist[0], mylist[i] = mylist[i], mylist[0]
+            else:
+                mylist[c[i]], mylist[i] = mylist[i], mylist[c[i]]
+            result.append(mylist[:])
+            c[i] += 1
+            i = 0
+        else:
+            c[i] = 0
+            i += 1
+
+
+# 가장 많은 알파벳 찾기
+import collections
+
+my_str = input().strip() # 입력
+answer = collections.Counter(my_str) # Counter(C 대문자), 딕셔너리 형태로 저장
+values = [i for i in answer.values()]
+values.sort(reverse=True)
+big = values[0]
+
+result = [i for i,k in answer.items() if big==k] # 간단히 하기
+result = ''.join(sorted(result))
+print(result)
+
+
+# flag OR for-else
+import math
+
+if __name__ == '__main__':
+    numbers = [int(input()) for _ in range(5)]
+    multiplied = 1
+    for number in numbers:
+        multiplied *= number
+        if math.sqrt(multiplied) == int(math.sqrt(multiplied)):
+            print('found')
+            break
+    else:
+        print('not found')
+
+# Swap
+a = 3
+b = 'avs'
+a,b = b,a
+
+# 이진 탐색
+import bisect
+mylist = [1,2,3,4,5,6]
+print(bisect.bisect(mylist,3))
+
+# 가장 큰 수, inf (양의 무한대), -inf (음의 마한대)
+min = float('inf')
+min > 1000000000
+
+n = int(input())
+for i in range(1, n):
+    print(' ' * (n-i) + '*' * (2 * i -1))
+for i in range(n,0,-1):
+    print(' ' * (n-i) + '*' * (2 * i -1))
