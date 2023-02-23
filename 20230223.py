1629833ad783eacd1fd4247ed6ae5cd46603dbdb
guseoh <guseoh@naver.com>
2023-02-24 00:45:25 +0900

    commit

---
+++ 20230223.py
@@ -0,0 +1,23 @@
+# croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+# a = input()
+
+# for i in croatia:
+#     a = a.replace(i, '*')
+# print(len(a))
+
+# while(True):
+#     cnt = 0
+#     n = input()
+#     n.lower()
+#     if n == '#':
+#         break
+#     for i in n:
+#         if i in 'aeiou':
+#             cnt += 1 
+#     print(cnt)
+    
+# vowels = ['a','e','i','o','u','A','E','I','O','U']
+# while True:
+#     line = input()
+#     if line == '#': break
+#     print(sum([(ch in vowels) for ch in line]))
