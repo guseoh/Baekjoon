87cb7a2b3305189f2e061c5b22c6af7673866dd6
guseoh <guseoh@naver.com>
2023-03-11 18:02:00 +0900

    commit

---
+++ 202303/20230311.py
@@ -0,0 +1,26 @@
+# arr = []
+# for _ in range(int(input())):
+#     arr.append(list(map(int, input().split())))
+    
+# arr.sort(key=lambda x:(x[0], x[1]))
+
+# for i in arr:
+#     print(str(i[0]) + " " + str(i[1]))
+
+# arr = []
+# for _ in range(int(input())):
+#     arr.append(list(map(int, input().split())))
+
+# arr.sort(key=lambda x:(x[1], x[0]))
+
+# for i in arr:
+#     print(str(i[0]) + " " + str(i[1]))
+
+# n = int(input())
+# a = list(map(int, input().split()))
+
+# a2 = sorted(list(set(a)))
+# dic = {a2[i] : i for i in range(len(a2))}
+# for i in a:
+#     print(dic[i], end=' ')
+
